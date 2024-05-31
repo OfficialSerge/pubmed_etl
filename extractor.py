e3a08f69e66bc007ce1c18aa3edabe0a4367fd2d
Sergiu Pod <sergiupod@gmail.com>
2024-05-31 11:15:12 -0500

    Formatted code

--- extractor.py
+++ extractor.py
@@ -1,6 +1,6 @@
 def main(gz, cores):
     files = os.listdir(gz)
-    gz_files = [file for file in files if '.gz' in file]
+    gz_files = [file for file in files if ".gz" in file]
 
     counter = 0
 
@@ -9,39 +9,33 @@ def main(gz, cores):
         lst = []
 
         for f in gz_files:
-            lst.append((f,gz))
-
+            lst.append((f, gz))
 
         with multiprocessing.Pool(processes=cores) as pool:
             results = pool.starmap(extractor, lst)
 
         files = os.listdir(gz)
-        gz_files = [file for file in files if '.gz' in file]
-
-
-
-
+        gz_files = [file for file in files if ".gz" in file]
 
 
 def extractor(filename, base_dir):
-    os.system(f'cd {base_dir} && gunzip {filename}')
+    os.system(f"cd {base_dir} && gunzip {filename}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import os
     import multiprocessing
     import argparse
 
-    parser = argparse.ArgumentParser(description='Parser')
-    parser.add_argument('-gz', dest='gz', type=str, help='Path to gz files directory')
-    parser.add_argument('-cores', dest='cores', type=str, help='Int, num cores')
+    parser = argparse.ArgumentParser(description="Parser")
+    parser.add_argument("-gz", dest="gz", type=str, help="Path to gz files directory")
+    parser.add_argument("-cores", dest="cores", type=str, help="Int, num cores")
 
     args = parser.parse_args()
     gz = args.gz
     cores = int(args.cores)
 
-    if gz[-1] != '/':
-        gz += '/'
+    if gz[-1] != "/":
+        gz += "/"
 
     main(gz, cores)
-
--- pyspark_parquet.py
+++ pyspark_parquet.py
@@ -9,7 +9,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--tname", help="schema.table_name. Like hm31.george_pipeline")
 parser.add_argument("--user", help="user of the db. Like hm31")
 parser.add_argument("--pas", help="password of the db")
-parser.add_argument("--path", help="path to the directory containing distributed parquet files")
+parser.add_argument(
+    "--path", help="path to the directory containing distributed parquet files"
+)
 
 
 args = parser.parse_args()
@@ -19,26 +21,21 @@ pas = args.pas
 parquet_path = args.path
 
 
-
-if parquet_path[-1] != '/':
-    parquet_path += '/'
-
-
+if parquet_path[-1] != "/":
+    parquet_path += "/"
 
 
 def insert_table(result, jdbc_url, jdbc_properties, tname):
-    result.repartition(10).write.jdbc(url=jdbc_url, table=tname, mode="overwrite",properties=jdbc_properties)
+    result.repartition(10).write.jdbc(
+        url=jdbc_url, table=tname, mode="overwrite", properties=jdbc_properties
+    )
 
 
-
-
-spark = SparkSession.builder \
-        .appName("parquet_unification") \
-        .config("spark.executor.memory", "10g") \
-        .getOrCreate()
-
-
-
+spark = (
+    SparkSession.builder.appName("parquet_unification")
+    .config("spark.executor.memory", "10g")
+    .getOrCreate()
+)
 
 
 df = spark.read.parquet(parquet_path)
@@ -48,7 +45,7 @@ spark.sparkContext.setLogLevel("WARN")
 start = time.time()
 
 df = df.repartition(1)
-df = df.dropDuplicates(['doi'])
+df = df.dropDuplicates(["doi"])
 init_count = df.count()
 mid = time.time()
 
@@ -73,32 +70,34 @@ sql_query = """
 
 result_df = spark.sql(sql_query)
 result_df = result_df.drop("RowRank")
-result_df = result_df.filter(col("doi") != '')
+result_df = result_df.filter(col("doi") != "")
 
 
 after_query = time.time()
 # print(f'run query in {after_query - mid}')s
 
-george_df = result_df.withColumn("has_abstract", F.when(F.length("abstract") > 0, 1).otherwise(0))
-george_df = george_df.withColumn("has_title", F.when(F.length("title") > 0, 1).otherwise(0))
-george_df = george_df.withColumn("has_mesh", F.when(F.length("mesh") > 2, 1).otherwise(0))
-george_df = george_df.withColumn("has_year", F.when(F.length("year") > 0, 1).otherwise(0))
+george_df = result_df.withColumn(
+    "has_abstract", F.when(F.length("abstract") > 0, 1).otherwise(0)
+)
+george_df = george_df.withColumn(
+    "has_title", F.when(F.length("title") > 0, 1).otherwise(0)
+)
+george_df = george_df.withColumn(
+    "has_mesh", F.when(F.length("mesh") > 2, 1).otherwise(0)
+)
+george_df = george_df.withColumn(
+    "has_year", F.when(F.length("year") > 0, 1).otherwise(0)
+)
 
-george_df = george_df.select("PMID", "doi", "has_abstract", "has_title", "has_mesh", "has_year")
+george_df = george_df.select(
+    "PMID", "doi", "has_abstract", "has_title", "has_mesh", "has_year"
+)
 george_df = george_df.withColumnRenamed("PMID", "pmid")
-
-
-
-
 
 
 jdbc_url = "jdbc:postgresql://valhalla.cs.illinois.edu:5432/ernieplus"
 
-jdbc_properties = {
-    "user": user,
-    "password": pas,
-    "driver": "org.postgresql.Driver"
-}
+jdbc_properties = {"user": user, "password": pas, "driver": "org.postgresql.Driver"}
 
 insert_table(george_df, jdbc_url, jdbc_properties, tname)
 
@@ -107,6 +106,3 @@ end = time.time()
 
 
 spark.stop()
-
-
-
